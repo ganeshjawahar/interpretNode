24adc88077fc9f1a3dbd855155be0a381caa33a9
ganeshjawahar <buzzganesh@gmail.com>
2017-05-23 00:28:13 +0530

    remove

--- embeddingmodel/node2vec/run.sh
+++
@@ -1,8 +0,0 @@
-p=(0.25 0.5 1 2 4)
-for i in "${p[@]}"
-do
-  for j in "${p[@]}"
-  do
-    ./node2vec -i:wa_edgelist -o:waemb/author-$i-$j -p:$i -q:$j -w -dr
-  done
-done
--- embeddingmodel/node2vec/word_run.sh
+++
@@ -1,2 +0,0 @@
-./node2vec -i:graph4_n2v -o:word/word-1-0.5 -p:1 -q:0.5 -w
-./node2vec -i:graph4_n2v -o:word/word-1-2 -p:1 -q:2 -w
