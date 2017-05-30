f07e9bc51ca0248d40c9a1f7a096d6c0d59dc726
ganeshjawahar <buzzganesh@gmail.com>
2017-05-30 17:47:30 +0530

    binsize code

--- feature_computation/wordcooccurrence/count_community.py
+++ feature_computation/wordcooccurrence/count_community.py
@@ -2,8 +2,24 @@ import scipy.io
 import numpy as np
 import random
 import math
+from scipy import stats
+import matplotlib.pyplot as plt
 random.seed(123)
 mat = scipy.io.loadmat('/home/ayushidalmia/interpretNode/graphs/dataset/POS.mat')
+
+def create_quantiles(arr, num_bins):
+  sorted(arr)
+  probs = [ (i/num_bins) for i in range(1, num_bins+1)]
+  bin_edges = stats.mstats.mquantiles(values, probs)
+  return bin_edges
+
+def get_class(num, bin_edges):
+  i = 0
+  for be in bin_edges[1:]:
+    if num < be:
+      return i
+    i = i + 1
+  return None
 
 graph=mat['group']
 nodes_2_c={}
@@ -17,12 +33,25 @@ for i,j,v in zip(cx.row, cx.col, cx.data):
 keys=nodes_2_c.keys()
 random.shuffle(keys)
 
+def run_to_file():
+  i=0
+  end=len(keys)
+  arr = []
+  while i<=end:
+    node = keys[i]
+    arr.append(len(nodes_2_c[node]))
+    i = i + 1
+  return arr
+arr = run_to_file()
+bin_edges = create_quantiles(arr, 10)
+
 def write_to_file(file, start, end):
   w=open(file, 'w')
   i=start
   while i<=end:
     node = keys[i]
-    w.write(node+"\t"+str(len(nodes_2_c[node]))+"\n")
+    cl = get_class(len(nodes_2_c[node]))
+    w.write(node+"\t"+str(cl)+"\n")
     i = i + 1
   w.close()
 
@@ -30,6 +59,7 @@ tr=int(len(keys)*0.7)
 dev=int(len(keys)*0.1)
 test=len(keys)-tr-dev
 baseDir = "/home/ayushidalmia/interpretNode/graphs/features/graph4/"
+run_to_file(keys)
 write_to_file(baseDir+'countCommunity_train', 0, tr-1)
 write_to_file(baseDir+'countCommunity_dev', tr, tr+dev-1)
 write_to_file(baseDir+'countCommunity_test', tr+dev, tr+dev+test-1)
